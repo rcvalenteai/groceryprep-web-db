49091e5e5191923687f38ddc31c3563ba77c596c
Noel Qiao <noelqiao88@gmail.com>
2021-05-03 23:05:42 -0400

    Add ingredient to recipe

--- src/pages/CreatorRecipe.js
+++ src/pages/CreatorRecipe.js
@@ -11,11 +11,12 @@ class CreatorRecipe extends React.Component {
         super(props);
         this.state = {
             creator: null,
-            currentMealPlan: null,
-            userMealPlans: [],
+            currentIngredient: null,
+            userIngredients: [],
             currentRecipe: null,
             userRecipes: [],
             recipeDetail: null,
+            quantity: 1.0,
             loading: true
         };
     }
@@ -45,14 +46,15 @@ class CreatorRecipe extends React.Component {
             let recipe_data = await fetch(get_recipe_url)
             let recipe_data_obj = await recipe_data.json()
 
-            // let get_recipelist_url = "https://lkt9ygcr5g.execute-api.us-east-2.amazonaws.com/beta/creator/recipe/";
-            // get_recipelist_url += sessionState.userUrl
-            // let recipelist_data = await fetch(get_recipelist_url)
-            // let recipelist_data_obj = await recipelist_data.json()
+            let get_ingredientlist_url = "https://lkt9ygcr5g.execute-api.us-east-2.amazonaws.com/beta/ingredients";
+            let ingredientlist_data = await fetch(get_ingredientlist_url)
+            let ingredientlist_data_obj = await ingredientlist_data.json()
+            console.log("ingredients")
+            console.log(ingredientlist_data_obj)
 
             this.setState({
-                // currentMealPlan: mealplanlist_data_obj.items[0],
-                // userMealPlans: mealplanlist_data_obj.items,
+                currentIngredient: ingredientlist_data_obj.items[0],
+                userIngredients: ingredientlist_data_obj.items,
                 currentRecipe: recipelist_data_obj.items[0],
                 userRecipes: recipelist_data_obj.items,
                 recipeDetail: recipe_data_obj.body,
@@ -73,14 +75,17 @@ class CreatorRecipe extends React.Component {
         })
     }
 
-    addRecipeToMealPlan = async () => {
-        let base_url = "https://lkt9ygcr5g.execute-api.us-east-2.amazonaws.com/beta/mealplan/recipe";
+    addIngredientToRecipe = async () => {
+        let base_url = "https://lkt9ygcr5g.execute-api.us-east-2.amazonaws.com/beta/ingredients/recipe";
+        console.log("add ingredient to recipe")
+        console.log(this.state.quantity)
         fetch(base_url, {
             //mode: 'no-cors',
             method: 'POST',
             body: JSON.stringify({
-                mealPlanUrl: this.state.currentMealPlan.location,
-                recipeUrl: this.state.currentRecipe.location
+                ingredientUrl: this.state.currentIngredient.location,
+                recipeUrl: this.state.currentRecipe.location,
+                quantity: this.state.quantity
             }),
             headers: {
                 'Accept': 'application/json',
@@ -89,45 +94,55 @@ class CreatorRecipe extends React.Component {
         }).then(response => response.json())
             .then(responseData => {
             }).then(() =>
-            this.fetchMealPlan()
+            this.fetchRecipe()
         )
     }
 
-    handleAddRecipeToMealPlan(event) {
+    handleAddIngredientToRecipe(event) {
         event.preventDefault()
-        this.addRecipeToMealPlan()
+        this.addIngredientToRecipe()
     }
 
-    handleSelectMealPlan(mealplan) {
+    handleSelectIngredient(ingredient) {
         this.setState({
-                currentMealPlan: mealplan.value
-            },
-            function () {
-                this.fetchMealPlan()
-            })
+                currentIngredient: ingredient.value
+            }
+        )
+    }
+
+    handleQuantity(quantity) {
+        console.log("handle quantity")
+        console.log(quantity)
+        this.setState({
+                quantity: quantity.target.value
+            }
+        )
     }
 
     handleSelectRecipe(recipe) {
         this.setState({
-            currentRecipe: recipe.value
-        },
+                currentRecipe: recipe.value
+            },
             function () {
-            this.fetchRecipe()
-        })
+                this.fetchRecipe()
+            })
     }
 
     render() {
         const loading = this.state.loading;
         const creator = this.state.creator
 
-        // let mealPlanLabelValue;
-        // let mealPlans = this.state.userMealPlans;
-        // mealPlanLabelValue = mealPlans.map((mealPlan, index) => {
-        //     let labelMealPlanValue = {}
-        //     labelMealPlanValue["label"] = mealPlan.name
-        //     labelMealPlanValue["value"] = mealPlan
-        //     return labelMealPlanValue
-        // })
+        let ingredientLabelValue;
+        let ingredients = this.state.userIngredients;
+        let dropDownValue = null
+        if (this.state.currentIngredient)
+            dropDownValue = this.state.currentIngredient.iname + " - " + this.state.currentIngredient.quantity + " " + this.state.currentIngredient.unit
+        ingredientLabelValue = ingredients.map((ingredient, index) => {
+            let labelIngredientValue = {}
+            labelIngredientValue["label"] = ingredient.iname + " - " + ingredient.quantity + " " + ingredient.unit
+            labelIngredientValue["value"] = ingredient
+            return labelIngredientValue
+        })
 
         let recipeLabelValue;
         let Recipes = this.state.userRecipes;
@@ -147,12 +162,20 @@ class CreatorRecipe extends React.Component {
                         <Dropdown options={recipeLabelValue} onChange={recipe => this.handleSelectRecipe(recipe)}
                                   value={this.state.currentRecipe.name}/>
                         <RecipeDetailDisplay recipe={this.state.recipeDetail}/>
-                        <br />
-                        <h2>TODO: Add Ingredients to Recipe:</h2>
-                        {/*<Dropdown options={mealPlanLabelValue} onChange={mealplan => this.handleSelectMealPlan(mealplan)}*/}
-                        {/*          value={this.state.currentMealPlan.name}/>*/}
-                        <br />
-                        <button onClick={e => this.handleAddRecipeToMealPlan(e)}>Add Ingredient to Recipe</button>
+                        <br/>
+                        <h2>Add Ingredients to Recipe:</h2>
+                        <Dropdown options={ingredientLabelValue}
+                                  onChange={ingredient => this.handleSelectIngredient(ingredient)}
+                                  value={dropDownValue}/>
+                        <br/>
+                        <form onSubmit={e => this.handleAddIngredientToRecipe(e)}>
+                            <label>
+                                Quantity To Add:
+                                <input type="number" min="0.01" step="0.001" value={this.state.quantity}
+                                       onChange={e => this.handleQuantity(e)}/>
+                            </label>
+                            <input type="submit" value="Add Ingredient to Recipe"/>
+                        </form>
                     </div>
                 )
             } else {
