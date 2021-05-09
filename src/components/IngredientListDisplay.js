94d998cfc3f2734558dac9fdf010fa583a6fc076
rcvalenteai <44619673+rcvalenteai@users.noreply.github.com>
2021-05-09 16:03:59 -0700

    added meal plan page. subscribe and details in progress

--- src/components/IngredientListDisplay.js
+++ src/components/IngredientListDisplay.js
@@ -1,12 +1,13 @@
-import React from 'react';
+
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import {Link} from "react-router-dom";
-import RecipeIngredientDisplay from "./RecipeIngredientDisplay";
 
 function IngredientListDisplay({ingredient}) {
+    console.log(ingredient);
     const name = ingredient.iname;
     const calories = ingredient.calories;
-    const quantity = ingredient.quantity;
+    const quantity_ing = ingredient.quantity;
     const orderQuantity = ingredient.order_quantity;
     const unit = ingredient.unit;
 
@@ -15,18 +16,59 @@ function IngredientListDisplay({ingredient}) {
         currency: 'USD',
     });
     const price = formatter.format(ingredient.price);
-    // render the UI
-    if (ingredient) return (
-        <div>
-            <h2>{name}</h2>
-            <h3>Order Quantity: {orderQuantity}</h3>
-            <h3>Calories: {calories}</h3>
-            <h3>Price: {price}</h3>
-            <h3>Quantity: {quantity} {unit}</h3>
-            <br></br>
-        </div>
-    )
-    return (null);
+
+    let addToOrder = (async (e) => {
+        e.preventDefault()
+        let sessionStorageString = window.sessionStorage.getItem('token')
+        let sessionStorage = JSON.parse(sessionStorageString)
+        let base_url = "https://lkt9ygcr5g.execute-api.us-east-2.amazonaws.com/beta/ingredients/order";
+        fetch(base_url, {
+            //mode: 'no-cors',
+            method: 'POST',
+            body: JSON.stringify({
+                groupUrl: sessionStorage.groupUrl,
+                ingredientUrl: ingredient.location,
+                quantity: parseInt(quantity_ing)
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(responseData => {
+            })
+    })
+
+    const [quantity, setQuantity] = useState(1)
+    let handleChange = (e) => {
+        e.preventDefault()
+        setQuantity(e.target.value)
     }
+
+
+    // render the UI
+    if (ingredient) {
+        return (
+            <div className='listItemDisplay'>
+                <h2>{name}</h2>
+                <h3>Order Quantity: {orderQuantity}</h3>
+                <h3>Calories: {calories}</h3>
+                <h3>Price: {price}</h3>
+                <h3>Quantity: {quantity} {unit}</h3>
+
+                <form onSubmit={e => addToOrder(e)}>
+                    <label>
+                        Add To Order:
+                        <input type="number" min="1" step="1" value={quantity}
+                               onChange={e => handleChange(e)}/>
+                    </label>
+                    <input type="submit" value="Add to Order"/>
+                </form>
+                <br></br>
+            </div>
+        )
+    }
+    return (null);
+}
 
 export default IngredientListDisplay;
--- src/components/MealPlanRecipeDisplay.js
+++ src/components/MealPlanRecipeDisplay.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import RecipeIngredientDisplay from "./RecipeIngredientDisplay";
 
 function MealPlanRecipeDisplay({recipe}) {
+    console.log(recipe)
     const name = recipe.name;
     const calories = recipe.calories;
     const description = recipe.description;
@@ -18,9 +19,15 @@ function MealPlanRecipeDisplay({recipe}) {
             <h2>{name}</h2>
             {descriptionTag}
             <br></br>
+            <a href="url">Details</a>
+            <div>
+                <button type="button">Subscribe</button>
+            </div>
         </div>
     )
     return (null);
     }
 
 export default MealPlanRecipeDisplay;
+
+
